ef4ede83e399a4f6d26f4ce7817273ec2ef32da0
Mxzah <mizahsecure123@outlook.com>
2026-03-10 10:51:00 -0400

    Fix les erreurs sur la prod

--- Rails/config/environments/production.rb
+++ Rails/config/environments/production.rb
@@ -57,7 +57,9 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   # Set host to be used by links generated in mailer templates.
-  config.action_mailer.default_url_options = { host: "example.com" }
+  config.action_mailer.default_url_options = { host: "resto.prod.system.shawinigan.info" }
+
+  Rails.application.routes.default_url_options[:host] = "resto.prod.system.shawinigan.info"
 
   # Specify outgoing SMTP server. Remember to add smtp/* credentials via bin/rails credentials:edit.
   # config.action_mailer.smtp_settings = {
